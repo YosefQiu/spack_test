45a155edb0b725e211fb70991eee51d327490742
YosefQiu <yongfengqyf@outlook.com>
2025-01-30 17:24:14 -0500

    [add] add ndarray repo

--- packages/sycl-ocean/package.py
+++ packages/sycl-ocean/package.py
@@ -1,4 +1,4 @@
-from spack import *
+from spack.package import *
 
 class SyclOcean(CMakePackage):
     """SYCL_OCEAN is a CMake-based project for ocean modeling using SYCL."""
@@ -11,12 +11,15 @@ class SyclOcean(CMakePackage):
     version('main', branch='master')  # 定义明确的版本
 
     # Add required dependencies
-    depends_on('intel-oneapi-compilers+nvidia')
-    depends_on('netcdf-c@4.9+mpi')
-    depends_on('vtk+mpi')
-    depends_on('yaml-cpp')
+    depends_on("intel-oneapi-compilers +nvidia", type=("build", "run"))
+    depends_on("ndarray +mpi +netcdf", type=("build", "link", "run"))
+    depends_on("vtk +mpi", type=("build", "link", "run"))
+    depends_on("yaml-cpp", type=("build", "link", "run"))
     
-    def install(self, spec, prefix):
-        with working_dir(self.build_directory):
-            make()
-            make("install")
+    def cmake_args(self):
+        args = []
+        args.append("-DCMAKE_C_COMPILER=icx")
+        args.append("-DCMAKE_CXX_COMPILER=icpx")
+        args.append("-DMPASO_USE_NDARRAY=ON")
+        args.append("-DNDARRAY_DIR=" + self.spec["ndarray"].prefix)
+        return args
